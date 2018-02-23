64fa2d7fa740e3384d52040a04b00426fa7f4fd1
ghajl <michaelhnn@gmail.com>
2018-02-24 00:10:05 +0200

    path 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(process.cwd(),  'public'),
-        publicPath: '/public/'
+        publicPath: '/public'
     },
     plugins: [
         new UglifyJSPlugin({
